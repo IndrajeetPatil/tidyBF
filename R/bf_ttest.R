0c05afef8145bb9224c68859be3d8d6e7be8916a
Indrajeet Patil <inderonline1988@gmail.com>
2020-12-04 09:30:23 +0100

    cran submission 0.4.2

--- R/bf_ttest.R
+++ R/bf_ttest.R
@@ -1,4 +1,4 @@
-#' @title Bayes Factor for *t*-test
+#' @title Bayes Factor for *t*-test and one-way ANOVA
 #' @rdname bf_ttest
 #'
 #' @details If `y` is `NULL`, a one-sample *t*-test will be carried out,
@@ -13,7 +13,7 @@
 #' @inheritParams ipmisc::long_to_wide_converter
 #' @inheritDotParams bf_extractor -bf.object
 #'
-#' @importFrom BayesFactor ttestBF
+#' @importFrom BayesFactor ttestBF anovaBF
 #' @importFrom rlang quo_is_null new_formula ensym enquo exec !!!
 #' @importFrom stats na.omit
 #' @importFrom dplyr pull
@@ -23,12 +23,35 @@
 #' \code{\link{bf_oneway_anova}}
 #'
 #' @examples
-#'
-#' # ------------------- two-samples tests -----------------------------------
-#'
+#' \donttest{
 #' # for reproducibility
 #' set.seed(123)
 #' library(tidyBF)
+#'
+#' # ----------------------- one-way ANOVA -----------------------------------
+#'
+#' # to get dataframe (between-subjects)
+#' bf_oneway_anova(
+#'   data = iris,
+#'   x = Species,
+#'   y = Sepal.Length,
+#'   bf.prior = 0.8,
+#'   output = "dataframe"
+#' )
+#'
+#' # to get expression (within-subjects) (needs `BayesFactor 0.9.12-4.3` or above)
+#' if (utils::packageVersion("BayesFactor") >= package_version("0.9.12-4.3")) {
+#'   bf_oneway_anova(
+#'     data = bugs_long,
+#'     x = condition,
+#'     y = desire,
+#'     subject.id = subject,
+#'     paired = TRUE,
+#'     output = "expression"
+#'   )
+#' }
+#'
+#' # ------------------- two-samples tests -----------------------------------
 #'
 #' # to get dataframe (between-subjects)
 #' bf_ttest(
@@ -60,6 +83,7 @@
 #'   test.value = 5.85,
 #'   output = "expression"
 #' )
+#' }
 #' @export
 
 # function body
@@ -71,7 +95,7 @@ bf_ttest <- function(data,
                      test.value = 0,
                      bf.prior = 0.707,
                      ...) {
-  # one-way or two-way table?
+  # one-sample or two-sample t-test?
   test <- ifelse(!rlang::quo_is_null(rlang::enquo(y)), "two.way", "one.way")
 
   # -------------------------- one-sample tests ------------------------------
@@ -88,6 +112,15 @@ bf_ttest <- function(data,
   # -------------------------- two-sample tests ------------------------------
 
   if (test == "two.way") {
+    # make sure both quoted and unquoted arguments are allowed
+    c(x, y) %<-% c(rlang::ensym(x), rlang::ensym(y))
+
+    # figure out which test to run based on the no. of levels of the IV
+    test.type <- ifelse(nlevels(as.factor(data %>% dplyr::pull({{ x }}))) < 3L, "t", "anova")
+
+    # should the data be spread?
+    if (test.type == "t") spread <- paired else spread <- FALSE
+
     # have a proper cleanup with NA removal
     data %<>%
       ipmisc::long_to_wide_converter(
@@ -96,24 +129,48 @@ bf_ttest <- function(data,
         y = {{ y }},
         subject.id = {{ subject.id }},
         paired = paired,
-        spread = paired
-      )
+        spread = spread
+      ) %>%
+      dplyr::mutate(rowid = as.factor(rowid))
 
-    # relevant arguments
-    if (isTRUE(paired)) bf.args <- list(x = data[[2]], y = data[[3]])
-    if (isFALSE(paired)) bf.args <- list(formula = rlang::new_formula(rlang::ensym(y), rlang::ensym(x)))
+    # relevant arguments for `BayesFactor` t-test
+    if (test.type == "t") {
+      .f <- BayesFactor::ttestBF
+      if (paired) args <- list(x = data[[2]], y = data[[3]], rscale = bf.prior, paired = paired)
+      if (!paired) args <- list(formula = rlang::new_formula(y, x), rscale = bf.prior, paired = paired)
+    }
 
-    # creating a BayesFactor object
+    # relevant arguments for `BayesFactor` one-way ANOVA
+    if (test.type == "anova") {
+      .f <- BayesFactor::anovaBF
+      if (paired) {
+        args <- list(
+          formula = rlang::new_formula(rlang::enexpr(y), rlang::expr(!!rlang::enexpr(x) + rowid)),
+          whichRandom = "rowid",
+          rscaleFixed = bf.prior,
+          rscaleRandom = 1
+        )
+      }
+      if (!paired) args <- list(formula = rlang::new_formula(y, x), rscaleFixed = bf.prior)
+    }
+
+    # creating a `BayesFactor` object
     bf_object <-
       rlang::exec(
-        .fn = BayesFactor::ttestBF,
-        rscale = bf.prior,
-        paired = paired,
+        .fn = .f,
         data = as.data.frame(data),
-        !!!bf.args
+        progress = FALSE,
+        !!!args
       )
   }
 
   # final return
   bf_extractor(bf_object, ...)
 }
+
+
+#' @rdname bf_ttest
+#' @aliases bf_ttest
+#' @export
+
+bf_oneway_anova <- bf_ttest
